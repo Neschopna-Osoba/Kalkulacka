fddde5f3973122cb440bfd0a40a91d9871c353aa
Petr Salavec <petr.salavec@student.gyarab.cz>
2018-10-31 11:20:38 +0100

    Initial Commit
    fix the numbers not disappering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null
+};
+
+function updateDisplay() {
+  const display = document.querySelector(".calculator-screen");
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+function inputDigit(digit) {
+  if (calculator.waitingForSecondOperand) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    if (calculator.displayValue == "0") {
+      calculator.displayValue = digit;
+    } else {
+      calculator.displayValue = calculator.displayValue + digit;
+    }
+  }
+}
+
+function handleOperator(nextOperator) {
+  if ((calculator.firstOperand = null)) {
+    calculator.firstOperand = parseFloat(calculator.displayValue);
+  } else if (calculator.operator) {
+    const result = performCalculation[calculator.displayValue](
+      calculator.firstOperand,
+      parseFloat(calculator.displayValue)
+    );
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+  "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
+  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
+  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+  "=": (firstOperand, secondOperand) => secondOperand
+};
+
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", event => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+
+  if (target.classList.contains("operator")) {
+    return;
+  }
+
+  if (target.classList.contains("all-clear")) {
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
